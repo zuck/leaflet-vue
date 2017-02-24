096c169ce1dcb00c2fdfda950bd3795e4eca4ab5
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2017-02-24 03:42:17 +0100

    First demo

---
+++ demo/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div id="side">
+      <h1><a href="https://github.com/zuck/leaflet-vue">leaflet-vue</a></h1>
+      <h2>Demo:</h2>
+      <ul>
+        <li>
+          <a href="#" @click="currentView='simple'">Simple map</a>
+        </li>
+      </ul>
+    </div>
+    <div id="container">
+      <component :is="currentView"></component>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Simple from './components/Simple'
+
+export default {
+  name: 'app',
+  components: {
+    Simple
+  },
+  data () {
+    return {
+      currentView: 'simple'
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+.leaflet-fake-icon-image-2x {
+  background-image: url(../../node_modules/leaflet/dist/images/marker-icon-2x.png);
+}
+.leaflet-fake-icon-shadow {
+  background-image: url(../../node_modules/leaflet/dist/images/marker-shadow.png);
+}
+
+@import "../../node_modules/leaflet/dist/leaflet.css";
+
+#side {
+  float:left;
+  width:200px;
+  padding:0 15px;
+  overflow-y:auto;
+}
+
+#container {
+  position:absolute;
+  top:0;
+  bottom:0;
+  right:0;
+  left:230px;
+}
+</style>
---
+++ demo/src/components/Simple.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <v-map :zoom="zoom" :min="minZoom" :max="maxZoom" :center="center">
+      <v-tilelayer :url="url" :attribution="attribution"></v-tilelayer>
+    </v-map>
+    <div class="form">
+      <label>ZOOM:</label>
+      <input type="number" v-model="zoom" :min="minZoom" :max="maxZoom">
+    </div>
+  </div>
+</template>
+
+<script>
+import LeafletVue from 'leaflet-vue';
+
+export default {
+  components: {
+    'v-map': LeafletVue.Map,
+    'v-tilelayer' :LeafletVue.TileLayer
+  },
+  data () {
+    return {
+      zoom: 5,
+      minZoom: 0,
+      maxZoom: 20,
+      center: [46.0637, 13.24458],
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution:'&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  width:100%;
+  height:100%;
+}
+
+.form {
+  position:absolute;
+  z-index:999;
+  top:10px;
+  left:80px;
+}
+</style>
